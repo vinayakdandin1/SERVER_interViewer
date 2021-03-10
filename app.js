3f39352c2d799619ee3cb94d66a3d77a758bb031
Benjamin megret <benji@MacBook-Pro-de-Benjamin.local>
2021-03-10 11:23:53 +0100

    push before deploy on heroku

--- app.js
+++ app.js
@@ -34,6 +34,9 @@ app.use(session({
     })
 }));
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // 👇 Start handling routes here
 // Contrary to the views version, all routes are controled from the routes/index.js
@@ -49,6 +52,10 @@ app.use("/api", jobRoutes)
 const cloudinaryRoute = require("./routes/cloudinary.route");
 app.use("/api", cloudinaryRoute)
 
+app.use((req, res, next) => {
+	// If no routes match, send them the React HTML.
+	res.sendFile(__dirname + "/public/index.html");
+});
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
